de56cdf249810e7f18a946ea862463914541b415
Suraj Mishra <surajmishragemini@gmail.com>
2024-04-13 21:11:49 -0400

    first_test

--- App.js
+++ App.js
@@ -5,14 +5,52 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/moduLes/routes.js";
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "../kanbas-node-server-app/Kanbas/Users/routes.js";
+import "dotenv/config";
+import session from "express-session";
 
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express()
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+ );
+
+ const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+
+//  const sessionOptions = {
+//     secret: "any string",
+//     resave: false,
+//     saveUninitialized: false,
+//   };
+//   app.use(
+//     session(sessionOptions)
+//   );
+  
 
 app.use(express.json());
+const port = process.env.PORT || 4000;
+
+UserRoutes(app);
 
 ModuleRoutes(app);
 
